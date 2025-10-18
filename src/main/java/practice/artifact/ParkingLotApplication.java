08bad324c2c886260ac8a8d84cb232ed4fb96d8a
ZenCoder18 <ankitkumarbeetroot1998@gmail.com>
2025-10-18 15:33:45 +0530

    first commit

---
+++ src/main/java/practice/artifact/ParkingLotApplication.java
@@ -0,0 +1,13 @@
+package practice.artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLotApplication.class, args);
+	}
+
+}
